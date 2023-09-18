bebd2d0a99f287de29bde3867ea3418fe3c52e0e
eslam97 <e01095097908@gmail.com>
2023-09-18 21:26:21 +0300

    footer

--- components/layouts/MainFooter.vue
+++ components/layouts/MainFooter.vue
@@ -3,9 +3,32 @@
 </script>
 
 <template>
-  <div class="bg-gray-800 text-white pt-16 pb-">
+  <div class="bg-gray-800 text-white pt-16 pb-9">
     <div class="container">
-      Hello
+      <div class="md:flex justify-between border-b border-gray-700 pb-9">
+        <div class="flex flex-col justify-between items-start mb-12 md:mb-0">
+           <img src="~/assets/images/logo-white.png" />
+          <ul class="list-none flex gap-9">
+            <li>ملاحظات عن الشحن</li>
+            <li>مركز المساعدة</li>
+            <li>الشروط والأحكام</li>
+          </ul>
+        </div>
+        <div class="flex flex-col justify-between gap-2">
+          <div class="flex gap-8 mb-16	">
+            <img src="~/assets/images/play-store.png" />
+            <img src="~/assets/images/app-store.png" />
+          </div>
+          <div class="flex justify-end gap-4 text-3xl">
+            <Icon name="teenyicons:instagram-solid" class="cursor-pointer" />
+            <Icon name="formkit:whatsapp" class="cursor-pointer" />
+            <Icon name="entypo-social:facebook-with-circle" class="cursor-pointer" />
+          </div>
+        </div>
+      </div>
+      <div class="pt-9 text-gray-700 text-xl text-center">
+        جميع الحقوق محفوظة لدى MYR GROUP
+      </div>
     </div>
   </div>
 </template>
