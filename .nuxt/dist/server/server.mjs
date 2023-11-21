b61f945df22003a42612c2a0cd2bfc800bc8eb7b
mahmoud-hus <mahmoud.hussein@dafa.sa>
2023-11-21 17:34:19 +0200

    gitignore

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/mahmoud/Projects/WBNR/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "auth" | "default" | "landing"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
